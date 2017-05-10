08ab0e9729cb7f7365e91db52d51c094edb7b2a0
Naohisa Sakamoto <naohisa@SKMT-MacBookPro13TB.local>
2017-05-11 00:20:03 +0900

    Modified.

--- Lib/Tuple.cpp
+++ Lib/Tuple.cpp
@@ -12,6 +12,45 @@ Tuple::Tuple( const size_t size ):
 {
 }
 
+Tuple::Tuple( const kvs::python::Object& object ):
+    kvs::python::Object( PyTuple_New( 1 ) )
+{
+    this->set( 0, object );
+}
+
+Tuple::Tuple(
+    const kvs::python::Object& object0,
+    const kvs::python::Object& object1 ):
+    kvs::python::Object( PyTuple_New( 2 ) )
+{
+    this->set( 0, object0 );
+    this->set( 1, object1 );
+}
+
+Tuple::Tuple(
+    const kvs::python::Object& object0,
+    const kvs::python::Object& object1,
+    const kvs::python::Object& object2 ):
+    kvs::python::Object( PyTuple_New( 3 ) )
+{
+    this->set( 0, object0 );
+    this->set( 1, object1 );
+    this->set( 2, object2 );
+}
+
+Tuple::Tuple(
+    const kvs::python::Object& object0,
+    const kvs::python::Object& object1,
+    const kvs::python::Object& object2,
+    const kvs::python::Object& object3 ):
+    kvs::python::Object( PyTuple_New( 4 ) )
+{
+    this->set( 0, object0 );
+    this->set( 1, object1 );
+    this->set( 2, object2 );
+    this->set( 3, object3 );
+}
+
 void Tuple::set( const size_t index, const kvs::python::Object& object )
 {
     object.increment();
